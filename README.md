08dc53b014adfe6f32cec87e2167109104f016ee
Pascal Allen <pascal.allen88@gmail.com>
2024-08-28 11:48:30 -0500

    initial exercise requirements complete

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# tst-exercise
+# TST coding exercise
+
+## Clone repository
+
+```bash
+cd <projects-parent-directory> && git clone https://github.com/pascalallen/tst-exercise.git
+```
+
+## Install JavaScript Dependencies
+
+```bash
+npm install
+```
+
+## Start dev server
+
+```bash
+npm start
+```
+
+You will find the site running at [http://localhost:3000/](http://localhost:3000/)
---
+++ src/App.js
@@ -0,0 +1,87 @@
+import { useState } from "react";
+
+function App() {
+  const [validationError, setValidationError] = useState('');
+  const [showValidationError, setShowValidationError] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setValidationError('');
+    setShowValidationError(false);
+    const formData = new FormData(e.currentTarget);
+    const emailAddress = formData.get('email_address')?.toString() ?? '';
+    const password = formData.get('password')?.toString() ?? '';
+    const confirmPassword = formData.get('confirm_password')?.toString() ?? '';
+    if (emailAddress.length <= 0) {
+      setValidationError('Email address is required');
+      setShowValidationError(true);
+      return;
+    }
+
+    if (password.length <= 0) {
+      setValidationError('Password is required');
+      setShowValidationError(true);
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setValidationError('Passwords do not match');
+      setShowValidationError(true);
+      return;
+    }
+
+    localStorage.setItem('auth_data', JSON.stringify({ emailAddress, password }));
+    setAuthenticated(true);
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('auth_data');
+    setAuthenticated(false);
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>TST coding exercise</h1>
+      </header>
+      <main>
+        <section>
+          {showValidationError && (
+              <div>
+                Validation error: {validationError}
+              </div>
+          )}
+          {authenticated ? (
+              <>
+                <div>Authenticated!</div>
+                <div>
+                  <button type="button" onClick={handleLogout}>Logout</button>
+                </div>
+              </>
+          ) : (
+              <form onSubmit={handleLogin}>
+                <div>
+                  <label htmlFor="email-address">Email address</label>
+                  <input id="email-address" type="email" name="email_address" placeholder="name@example.com"/>
+                </div>
+                <div>
+                  <label htmlFor="password">Password</label>
+                  <input id="password" type="password" name="password" placeholder="Password"/>
+                </div>
+                <div>
+                  <label htmlFor="confirm-password">Confirm Password</label>
+                  <input id="confirm-password" type="password" name="confirm_password" placeholder="Confirm Password"/>
+                </div>
+                <div>
+                  <button type="submit">Register</button>
+                </div>
+              </form>
+          )}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
